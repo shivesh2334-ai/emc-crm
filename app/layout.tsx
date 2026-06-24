4cd33aca2b018130b8c9ee508cac691713797570
Shivesh <drshivesh@gmail.com>
2026-06-24 13:40:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
